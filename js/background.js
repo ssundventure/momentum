c8432ca2679e3bc7f39c74189d07d9c3d669fd55
ssundventure <ssundventure@gmail.com>
2025-03-10 01:09:20 +0900

    디자인 수정

--- js/background.js
+++ js/background.js
@@ -4,7 +4,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-
 bgImage.src = `/img/${chosenImage}`;
 
 document.body.appendChild(bgImage);

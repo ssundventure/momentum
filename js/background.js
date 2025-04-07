8212366ff6083527aa17690400ebc77aeb36877e
ssundventure <ssundventure@gmail.com>
2025-04-07 23:59:00 +0900

    사진 출력 로직 수정

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
-//const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
-const images = ["2.jpeg"];
+//const images = ["2.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 

c16779d58ec59c0d26bd5a719b4d1d1a3a354bb7
ssundventure <ssundventure@gmail.com>
2025-04-23 01:09:22 +0900

    goal 로직 수정

--- js/goal.js
+++ js/goal.js
@@ -1,4 +1,6 @@
-//form
+import { HIDDEN_CLASSNAME } from "../util/utility";
+
+//goal-form
 const goalFormWrapper = document.querySelector("#goal-form-wrapper");
 const goalForm = document.querySelector("#goal-form");
 const goalInput = document.querySelector("#goal-form input");
@@ -7,7 +9,7 @@ const goalDisplayWrapper = document.querySelector("#goal-display-wrapper");
 const goalDisplayText = document.querySelector("#goal-display-text");
 const goalCheckbox = document.querySelector("#goal-checkbox");
 const checkIcon = document.querySelector(".checked-icon");
-//dropdown
+//goal-dropdown
 const dropdownToggle = document.querySelector("#goal-dropdown-toggle");
 const dropdown = document.querySelector("#goal-dropdown");
 const dropdownNewBtn = document.querySelector("#dropdown-new-btn");
@@ -15,7 +17,6 @@ const dropdownEditBtn = document.querySelector("#dropdown-edit-btn");
 const dropdownClearBtn = document.querySelector("#dropdown-clear-btn");
 
 //constants
-const HIDDEN_CLASSNAME = "hidden";
 const CANCEL_LINE_CLASSNAME = "cancel-line";
 const GOAL_KEY = "goal";
 const GOAL_CHECKED_KEY = "goal_checked";
@@ -23,8 +24,15 @@ const GOAL_CHECKED_KEY = "goal_checked";
 let goalChecked = false;
 
 /** goal */
-function saveGoal(newGoal) {
-    localStorage.setItem(GOAL_KEY, newGoal);
+function saveGoal(newGoal, ttl) {
+    console.trace("who caleed?");
+    const now = new Date();
+    const item = {
+        value: newGoal,
+        expires: now.getTime() + ttl,
+    };
+
+    localStorage.setItem(GOAL_KEY, JSON.stringify(item));
 }
 
 function removeGoal() {
@@ -40,9 +48,6 @@ function paintGoal(newGoal) {
 }
 
 function paintGoalInput(existedGoal) {
-    console.log(`existedGoal : ${existedGoal}`);
-    console.log(`type of existedGoal : ${typeof existedGoal}`);
-
     goalDisplayWrapper.classList.add(HIDDEN_CLASSNAME);
     goalFormWrapper.classList.remove(HIDDEN_CLASSNAME);
 
@@ -54,27 +59,28 @@ function paintGoalInput(existedGoal) {
 function handleGoalSubmit(event) {
     event.preventDefault();
     const newGoal = goalInput.value;
-    goalInput.value = null;
+    console.log("nw goal :"+newGoal);
     paintGoal(newGoal);
-    saveGoal(newGoal);
+    //2번쨰인자 : 밀리초단위
+    saveGoal(newGoal, (24 * 60) * 60 * 1000);
+    goalInput.value = null;
 }
 
 /**goal check */
 function saveGoalChecked(isChecked) {
-    localStorage.setItem(GOAL_CHECKED_KEY, isChecked)
+    localStorage.setItem(GOAL_CHECKED_KEY, JSON.stringify(isChecked))
 }
 
 function paintGoalCheckbox() {
-    console.log(`paint checkbox. 체크여부: ${goalChecked}`);
     if (goalChecked) {
-        //checked 표시
+        //checked 
         checkIcon.classList.remove(HIDDEN_CLASSNAME);
         goalDisplayText.classList.add(CANCEL_LINE_CLASSNAME);
         dropdownNewBtn.classList.remove(HIDDEN_CLASSNAME);
         dropdownEditBtn.classList.add(HIDDEN_CLASSNAME);
     }
     else {
-        //checked 표시 해제
+        //NOT checked
         checkIcon.classList.add(HIDDEN_CLASSNAME);
         goalDisplayText.classList.remove(CANCEL_LINE_CLASSNAME);
         dropdownNewBtn.classList.add(HIDDEN_CLASSNAME);
@@ -87,10 +93,9 @@ function handleGoalCheckbox() {
 
     paintGoalCheckbox();
     saveGoalChecked(goalChecked);
-
 }
-/**goal dropdown */
 
+/**goal dropdown */
 function handleDropdownToggle() {
     dropdown.classList.toggle(HIDDEN_CLASSNAME);
 
@@ -101,17 +106,16 @@ function handleDropdownClear() {
 }
 
 function handleDropdownEdit() {
-    console.log(`edit goal -> ${savedGoal}`)
-    paintGoalInput(savedGoal);
+    paintGoalInput(savedGoal.value);
 }
 
 //로직 수정 필요
 function handleDropdownNew() {
     goalChecked = false;
     removeGoal();
+    paintGoalCheckbox();
     paintGoalInput();
 }
-
 
 
 goalForm.addEventListener("submit", handleGoalSubmit);
@@ -125,14 +129,24 @@ dropdownEditBtn.addEventListener("click", handleDropdownEdit);
 dropdownNewBtn.addEventListener("click", handleDropdownNew);
 
 
-const savedGoal = localStorage.getItem(GOAL_KEY);
+const savedGoal = JSON.parse(localStorage.getItem(GOAL_KEY));
+//const goalExpireTime = JSON.parse(localStorage.getItem(GOAL_KEY)).expires;
 const savedGoalChecked = JSON.parse(localStorage.getItem(GOAL_CHECKED_KEY));
 
+
 if (savedGoalChecked !== null) {
-    console.log(`savedGoalChecked : ${savedGoalChecked}`);
     goalChecked = savedGoalChecked;
 }
 
 if (savedGoal !== null) {
-    paintGoal(savedGoal);
+    const now = new Date();
+    //time check
+    if (now.getTime() < savedGoal.expires) {
+        paintGoal(savedGoal.value);
+    } else {
+        removeGoal();
+    }
+
 }
+
+

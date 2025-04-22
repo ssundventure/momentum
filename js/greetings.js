3fe458e11e54feb260cbed1b3c8ea139024c67f5
ssundventure <ssundventure@gmail.com>
2025-04-23 01:09:05 +0900

    greeting 로직, css 수정

--- js/greetings.js
+++ js/greetings.js
@@ -1,13 +1,18 @@
+import { HIDDEN_CLASSNAME } from "../util/utility";
+
+const login = document.querySelector("#login");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting-text");
 
-const HIDDEN_CLASSNAME = "hidden";
+const goal = document.querySelector("#goal");
+const clock = document.querySelector("#clock");
+
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  login.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
@@ -16,13 +21,26 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   greeting.innerText = `Hello, ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+
+  clock.classList.remove(HIDDEN_CLASSNAME);
+  goal.classList.remove(HIDDEN_CLASSNAME);
+  login.classList.add(HIDDEN_CLASSNAME);
+
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
+
 if (savedUsername === null) {
+  goal.classList.add(HIDDEN_CLASSNAME);
+  clock.classList.add(HIDDEN_CLASSNAME);
+
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello! What's your name?`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
+
 }
